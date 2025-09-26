26ae101c2a4d1961170459b4fff604075298be9c
chugani05 <chugani205@gmail.com>
2025-09-26 18:55:30 +0100

    progress with theme; start button component

--- calc/app/index.tsx
+++ calc/app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { myColors } from '@/styles/colors';
+import { ThemeContext } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Switch, View } from "react-native";
 
 export default function Index() {
+  const [theme, setTheme] = useState("light");
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <View style={theme === 'light' ? StyleSheet.container : [style.container, {backgroundColor: '#000'}]}>
+        <StatusBar style="auto" />
+        <Switch
+          value={theme === 'light'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+      </View>
+    </ThemeContext.Provider>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: myColors.light,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
