905d68c8cf92b4e38f40e3a80b534378a2f95102
chugani05 <chugani205@gmail.com>
2025-09-30 18:42:23 +0100

    progress

--- calc/styles/Colors.tsx
+++ calc/styles/Colors.tsx
@@ -1,22 +1,32 @@
 export const myColors = {
-    light: '#F1F2F3',
-    dark: '#17171C',
+    light: '#FAFAFA',
+    dark: '#2C2C2C',
     blue: '4B5EFC',
     btnGray: '4E505F',
     btnDark: '#2E2F38',
     grey: '#747477',
     black: '#000000',
     white: '#FFFFFF',
-    result: '#46D5B2',
+    result: '#A8E6CF',
 }
 
 
-//   "light": "#FAFAFA",           Fondo claro pastel
-//   "dark": "#2C2C2C",            Fondo oscuro pastel
-//   "textLight": "#333333",       Texto en modo claro
-//   "textDark": "#F1F1F1",        Texto en modo oscuro
-//   "buttonBgLight": "#E0C3FC",   Botones pastel en modo claro
-//   "buttonBgDark": "#6048F8",    Botones pastel en modo oscuro
-//   "buttonText": "#FFFFFF",      Texto en botones
-//   "grey": "#A8A8A8",            Texto secundario
-//   "result": "#A8E6CF"           Resultado en tono pastel
+    // Light pallette
+    // bgLight: '#FDF6F0',
+    // screenLight: '#E8F0FE',
+    // textLight: '#3B3B3B',
+    // numLight: '#FFE0AC',
+    // opLight: '#FFB4B4',
+    // equalLight: '#CBA6F7',
+    // clearLight: '#F7A4A4',
+    // resultLight: '#A8D8B9',
+
+    // Dark pallette
+    // bgDark: "#2C2C34",
+    // screenDark: "#3E4157",
+    // textDark: "#F5F5F5",
+    // numDark: "#FFD8A8",
+    // opDark: "#FF9E9E",
+    // equalDark: "#B68CFF",
+    // clearDark: "#FF7B7B",
+    // resultDark: "#81C784",
